28ae06106266be3178fc23903cc8d83e8d2af5b4
Gaurav Kumar <gauravkumar0998@gmail.com>
2020-09-07 00:05:38 +0530

    Add project files.

---
+++ IntroductionToLibraries/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using static MathematicalOperations.Operations;
+using static System.Console;
+
+namespace IntroductionToLibraries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteLine("Enter first value:");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //WriteLine("Enter Second value:");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //WriteLine($"The output of the given value is : {Add(a,b)}");
+
+            ////Files
+            //DirectoryInfo dirInfo = new DirectoryInfo("C:\\Users\\gauravr\\Documents\\TestFolder");
+            //DirectoryInfo [] folders = dirInfo.GetDirectories();
+
+            //foreach (var item in folders)
+            //{
+            //    WriteLine(item.Name);
+            //    WriteLine("------------------------");
+            //    foreach(var file in item.GetFiles())
+            //    {
+            //        WriteLine(file.Length); //size of files
+            //    }
+            //}
+
+            WriteLine("Enter the Directory Path");
+            string path = Console.ReadLine();
+
+            //Files
+            DirectoryInfo dirInfo1 = new DirectoryInfo(path);
+            bool value = dirInfo1.Exists;
+            if (value.Equals(true))
+            {
+                DirectoryInfo[] folder = dirInfo1.GetDirectories();
+
+                foreach (var item in folder)
+                {
+                    WriteLine(item.Name);
+                    WriteLine("------------------------");
+                    foreach (var file in item.GetFiles())
+                    {
+                        WriteLine(file.Length); //size of files
+                    }
+                }
+            }
+            else
+            {
+                WriteLine("Incorrect Path");
+            }
+            ReadLine();
+        }
+    }
+}
---
+++ MathematicalOperations/Operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathematicalOperations
+{
+    public static class Operations
+    {
+        public static int Add(int a , int b)
+        {
+            return a + b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Divison(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
